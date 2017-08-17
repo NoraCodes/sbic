d0b47242bd6aea76929df320eef9dad19a1af015
SilverWingedSeraph <lfstindall@gmail.com>
2017-08-17 14:33:49 -0700

    Don't panic on things that aren't inconciveable

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::env::args;
 
+
+fn efail(s: String) -> ! {
+    println!("{}", s);
+    std::process::exit(1);
+}
+
 #[derive(Debug, Clone, Deserialize)]
 struct Configuration {
     pub input: Vec<u32>,
@@ -19,14 +25,14 @@ fn read_config(filename: &Path) -> Configuration {
     let mut config = String::new();
     let mut f = match File::open(filename) {
         Ok(f) => f,
-        Err(e) => {panic!(format!("Failed to open file {}: {}", filename.to_string_lossy(), e));}
+        Err(e) => {efail(format!("Failed to open file {}: {}", filename.to_string_lossy(), e));}
     };
 
     f.read_to_string(&mut config).expect("Could not read from file.");
 
     let config: Configuration = match toml::from_str(&config) {
         Ok(c) => c,
-        Err(e) => {panic!(format!("Failed to parse configuration file: {}", e))}
+        Err(e) => {efail(format!("Failed to parse configuration file: {}", e))}
     };
 
     config
@@ -35,7 +41,7 @@ fn read_config(filename: &Path) -> Configuration {
 fn main() {
     let args: Vec<_> = args().collect();
     if args.len() != 2 {
-        panic!("You must specify a configuration file!")
+        efail("You must specify a configuration file!".into())
     }
 
     let config = read_config(&Path::new(&args[1]));
