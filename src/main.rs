fcbd52d874255727339f937d33699aac874fd47c
SilverWingedSeraph <lfstindall@gmail.com>
2017-09-23 16:46:47 -0500

    Revamp with CLAP

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,126 @@
 extern crate sbrain;
-extern crate toml;
-#[macro_use]
-extern crate serde_derive;
-use std::path::Path;
+extern crate clap;
+
+use std::path::{Path, PathBuf};
 use std::io::prelude::*;
 use std::fs::File;
 use std::env::args;
+use std::error::Error;
 
+use clap::{App, Arg};
 
+/// Exit from the program on unrecoverable failure.
 fn efail(s: String) -> ! {
-    println!("{}", s);
+    println!("FATAL: {}", s);
     std::process::exit(1);
 }
 
-#[derive(Debug, Clone, Deserialize)]
-struct Configuration {
-    pub input: Vec<u32>,
-    pub source: String,
-    pub max_runtime: Option<u32>
+
+struct ProgramInputs {
+    sourcefile: PathBuf,
+    inputfile: Option<PathBuf>,
+    cycles: Option<u32>,
+    source: String,
+    input: Vec<u8>,
+    bytes_output: bool,
+    verbose: bool,
 }
 
-fn read_config(filename: &Path) -> Configuration {
 
-    let mut config = String::new();
-    let mut f = match File::open(filename) {
-        Ok(f) => f,
-        Err(e) => {efail(format!("Failed to open file {}: {}", filename.to_string_lossy(), e));}
-    };
+fn get_inputs() -> ProgramInputs {
 
-    f.read_to_string(&mut config).expect("Could not read from file.");
+    let matches = App::new("SBrain Interpreter and Compiler")
+                          .version(env!("CARGO_PKG_VERSION"))
+                          .author("Leo Tindall <lfstindall@gmail.com>")
+                          .about("Executes and compiles SBrain programs")
+                          .arg(Arg::with_name("SOURCE")
+                               .help("The source file to operate on")
+                               .required(false)
+                               .index(1))
+                          .arg(Arg::with_name("input")
+                               .short("i")
+                               .long("input")
+                               .help("The file to use for the program's input")
+                               .value_name("FILE")
+                               .takes_value(true))
+                          .arg(Arg::with_name("cycles")
+                               .short("c")
+                               .long("cycles")
+                               .help("The maximum number of cycles for which to run")
+                               .value_name("CYCLES")
+                               .takes_value(true))
+                          .arg(Arg::with_name("bytes_output")
+                               .short("b")
+                               .long("bytes-output")
+                               .help("Display output as decimal bytes, not lossy text."))
+                          .arg(Arg::with_name("verbose")
+                               .short("v")
+                               .long("verbose")
+                               .help("Display more info."))
+                          .get_matches();
 
-    let config: Configuration = match toml::from_str(&config) {
-        Ok(c) => c,
-        Err(e) => {efail(format!("Failed to parse configuration file: {}", e))}
-    };
+    let sourcefile: PathBuf = matches.value_of("SOURCE")
+                            .expect("You must provide a source file")
+                            .into();
 
-    config
+    let inputfile: Option<PathBuf> = matches.value_of("input")
+                            .and_then(|s| Some(s.into()));
+
+    let cycles = matches.value_of("cycles")
+        .and_then(
+            |s| Some(u32::from_str_radix(s, 10).expect("Invalid number of cycles.")));
+    
+    // Now read the actual files
+    let mut source = String::new();
+    File::open(&sourcefile)
+        .unwrap_or_else(|e| efail(format!("Couldn't open {}: {}", 
+                                   sourcefile.display(),
+                                   e.description())))
+        .read_to_string(&mut source)
+        .unwrap_or_else(|e| efail(format!("Couldn't read {}: {}",
+                                   sourcefile.display(),
+                                   e.description())));
+
+    // Input will be an empty vec unless some is provided.
+    let mut input = Vec::<u8>::new();
+    if let Some(inputfile) = inputfile.clone() {
+        File::open(&inputfile)
+            .unwrap_or_else(|e| efail(format!("Couldn't open {}: {}",
+                                       inputfile.display(),
+                                       e.description())))
+            .read_to_end(&mut input)
+            .unwrap_or_else(|e| efail(format!("Couldn't read {}: {}",
+                                       inputfile.display(),
+                                       e.description())));
+    }
+    
+    ProgramInputs {
+        sourcefile: sourcefile,
+        inputfile: inputfile,
+        cycles: cycles,
+        source: source,
+        input: input,
+        bytes_output: matches.is_present("bytes_output"),
+        verbose: matches.is_present("verbose"),
+    }
+
+
 }
 
 fn main() {
-    let args: Vec<_> = args().collect();
-    if args.len() != 2 {
-        efail("You must specify a configuration file!".into())
-    }
+    let inputs = get_inputs();
 
-    let config = read_config(&Path::new(&args[1]));
-
-    let mut machine = sbrain::SBrainVM::new(Some(config.input.clone()));
-    let (program_tape, data_tape) = sbrain::source_to_tapes(&config.source);
+    let mut machine = sbrain::SBrainVM::new(
+        Some(inputs.input.iter().map(|n| *n as u32).collect()));
+    let (program_tape, data_tape) = sbrain::source_to_tapes(&inputs.source);
     machine.load_program(&program_tape).unwrap();
     machine.load_data(&data_tape).unwrap();
 
-    machine.run(config.max_runtime);
+    machine.run(inputs.cycles);
 
-    println!("{:?}", machine.get_output());
+    if inputs.bytes_output {
+        println!("{:?}", machine.get_output());
+    } else {
+        println!("{}", &sbrain::tape_to_string(machine.get_output()));
+    }
 }
